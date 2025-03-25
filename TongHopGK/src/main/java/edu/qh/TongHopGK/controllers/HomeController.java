0d9fe13786feaac1c71a3bb5cdda8514df5479d6
QuHung-0 <hung.nlq.64cntt@ntu.edu.vn>
2025-03-25 09:30:28 +0700

    Push Lan 3

--- TongHopGK/src/main/java/edu/qh/TongHopGK/controllers/HomeController.java
+++ TongHopGK/src/main/java/edu/qh/TongHopGK/controllers/HomeController.java
@@ -1,16 +1,15 @@
 package edu.qh.TongHopGK.controllers;
 
 import java.util.ArrayList;
-import java.util.List;
-
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import edu.qh.TongHopGK.model.SinhVien;
 
 
 public class HomeController
 {
-	ArrayList<SinhVien> dsSV;
+	ArrayList<SinhVien> dsSV = new ArrayList<>();
 	
 	@GetMapping("/")
 	public String home(Model model)
---
+++ TongHopGK/src/main/java/edu/qh/TongHopGK/model/SinhVien.java
@@ -0,0 +1,46 @@
+package edu.qh.TongHopGK.model;
+
+public class SinhVien
+{
+	private String mssv;
+	private String hoTen;
+	private double diemTB;
+
+
+	public SinhVien(String mssv, String hoTen, double diemTB)
+	{
+		this.mssv = mssv;
+		this.hoTen = hoTen;
+		this.diemTB = diemTB;
+	}
+
+	public String getMssv()
+	{
+		return mssv;
+	}
+
+	public void setMssv(String mssv)
+	{
+		this.mssv = mssv;
+	}
+
+	public String getHoTen()
+	{
+		return hoTen;
+	}
+
+	public void setHoTen(String hoTen)
+	{
+		this.hoTen = hoTen;
+	}
+
+	public double getDiemTB()
+	{
+		return diemTB;
+	}
+
+	public void setDiemTB(double diemTB)
+	{
+		this.diemTB = diemTB;
+	}
+}
