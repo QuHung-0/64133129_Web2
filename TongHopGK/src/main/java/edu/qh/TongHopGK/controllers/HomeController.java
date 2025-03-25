fac10f94eb776c1caf1a6db9334d25abb71a36b7
QuHung-0 <hung.nlq.64cntt@ntu.edu.vn>
2025-03-25 10:37:10 +0700

    Push Lan 6

--- TongHopGK/src/main/java/edu/qh/TongHopGK/controllers/HomeController.java
+++ TongHopGK/src/main/java/edu/qh/TongHopGK/controllers/HomeController.java
@@ -1,38 +1,63 @@
 package edu.qh.TongHopGK.controllers;
 
 import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import edu.qh.TongHopGK.model.SinhVien;
 
-
+@Controller
 public class HomeController
 {
-	ArrayList<SinhVien> dsSV = new ArrayList<>();
-	
+	private List<SinhVien> dssv = new ArrayList<>();
+
+	public HomeController()
+	{
+		dssv.add(new SinhVien("SV001", "Trần Hải Đăng", 8.5));
+		dssv.add(new SinhVien("SV002", "Lưu Thanh Hà", 7.8));
+		dssv.add(new SinhVien("SV003", "Nguyễn Đắc Cẩn", 9.0));
+	}
+
 	@GetMapping("/")
 	public String home(Model model)
 	{
+		model.addAttribute("title", "Trang Chủ");
 		return "index";
 	}
 
 	@GetMapping("/about")
-	public String about()
+	public String about(Model model)
 	{
+		model.addAttribute("title", "About");
 		return "about";
 	}
 
-	@GetMapping("/List")
-	public String List()
+	@GetMapping("/studentList")
+	public String studentList(Model model)
 	{
-		return "List";
+		model.addAttribute("dssv", dssv);
+		return "studentList";
 	}
 
-	@GetMapping("/Addnew")
-	public String Addnew()
+	@GetMapping("/addNew")
+	public String addNewForm(Model model)
 	{
-		return "Addnew";
+		return "addnew";
 	}
 
+	@PostMapping("/addNew")
+	public String addNewStudent(@RequestParam("mssv") String mssv, @RequestParam("hoTen") String hoTen,
+			@RequestParam("diemTB") double diemTB, Model model)
+	{
+
+		SinhVien sv = new SinhVien(mssv, hoTen, diemTB);
+		dssv.add(sv);
+		return "redirect:/studentList";
+	}
 }
